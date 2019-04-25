7ca9db16a04ce7f921720e165542bf52bdea611f
niranjan2013 <42775845+niranjan2013@users.noreply.github.com>
2019-04-25 22:42:24 +0530

    Update and rename CNA Application.java to DemoStarterApplication.java

--- src/main/java/com/dxc/cna/demoStarterProj/DemoStarterApplication.java
+++ src/main/java/com/dxc/cna/demoStarterProj/DemoStarterApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CNA Application {
+public class DemoStarterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CNA Application.class, args);
+		SpringApplication.run(DemoStarterApplication.class, args);
 	}
 }
