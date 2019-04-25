b5c187c01f6aeb8b8b99451f95318715488745bc
vcap <vcap@ebb2554c-ee0c-4ca2-5002-3e5f>
2019-04-25 17:14:24 +0000

    Committed all the changes including additions

---
+++ src/main/java/com/dxc/cna/demoStarterProj/CNA Application.java
@@ -0,0 +1,12 @@
+package com.dxc.cna.demoStarterProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CNA Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CNA Application.class, args);
+	}
+}
